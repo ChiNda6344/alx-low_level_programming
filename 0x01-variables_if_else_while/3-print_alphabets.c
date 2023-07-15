63bf30048e9806201491ecd6235c8f40134ac7cc
ChiNda6344 <chindaconfidence70@gmail.com>
2023-07-14 20:16:01 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets in UPPERCASE
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char b;
+	char c;
+
+	b = 'a';
+	c = 'A';
+	while
+		(b <= 'z') {
+			putchar (b);
+			b++;
+			}
+	while
+		(c <= 'Z') {
+			putchar (c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
