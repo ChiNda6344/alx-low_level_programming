74b3f35c345ba44ad528c7098587423c80f2a888
ChiNda6344 <chindaconfidence70@gmail.com>
2023-07-14 21:41:38 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints single didgit numbers of baste ten from 0
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while
+		(i < 10) {
+			putchar(i + '0');
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
