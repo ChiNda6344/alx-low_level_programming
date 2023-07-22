7ac0f68ffc12756921c162cccfff27f55b6bb231
ChiNda6344 <chindaconfidence70@gmail.com>
2023-07-21 19:33:37 -0700

    multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * mul - function checks for 0 through digits
- * @a: a - variable
- * @b: b - variable
+ * mul - function checks for a digit (0 through)
+ * @a: a - var
+ * @b: b - var
  * Return: 0 (success)
  */
-
 int mul(int a, int b)
 {
 	int mul:
