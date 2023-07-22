c0336af41030a3098a92109386a773589d327b24
ChiNda6344 <chindaconfidence70@gmail.com>
2023-07-21 19:53:02 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function checks for checks for a digit
+ * Return: 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		_putchar(b);
+	}
+		_putchar('\n');
+}
