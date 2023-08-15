bf01c2f365ed126034a010fe5076416fe30000a9
ChiNda6344 <chindaconfidence70@gmail.com>
2023-08-15 14:59:08 -0700

    2-print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,12 +6,10 @@
  * print_dog - prints a struct dog
  * @d: struct dog to print
  */
-
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
